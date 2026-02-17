8c75793b47d000b0641e7bf6fa58f422228abf88
Indresh Singh <indreshsingh@192.168.1.15>
2026-02-18 04:29:03 +0530

    Configure trust proxies and force HTTPS for secure subdomain

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+
         \Illuminate\Support\Facades\Event::subscribe(\App\Listeners\AuditLogSubscriber::class);
 
         \Illuminate\Support\Facades\Blade::directive('indian_format', function ($expression) {
